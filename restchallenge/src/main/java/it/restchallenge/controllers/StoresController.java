bde6fc197eb1b84df97e77021aca24693d6fe699
Lorenzo603 <lorenzo.furrer@gmail.com>
2017-06-24 10:08:59 +0200

    Datetime conversion fix. get stores for ID request mapping

--- restchallenge/src/main/java/it/restchallenge/controllers/StoresController.java
+++ restchallenge/src/main/java/it/restchallenge/controllers/StoresController.java
@@ -3,8 +3,8 @@ package it.restchallenge.controllers;
 import it.restchallenge.data.StoreData;
 import it.restchallenge.facades.StoreFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Lfurrer on 24/06/2017.
@@ -15,9 +15,11 @@ public class StoresController {
     @Autowired
     private StoreFacade storeFacade;
 
-    @RequestMapping("/stores")
-    public StoreData getStore() {
-        return storeFacade.getStoreById("");
+    @RequestMapping(value = "/stores", method = RequestMethod.GET)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.OK)
+    public StoreData getStore(@RequestParam String id) {
+        return storeFacade.getStoreById(id);
     }
 
 }
--- restchallenge/src/main/java/it/restchallenge/data/StoreData.java
+++ restchallenge/src/main/java/it/restchallenge/data/StoreData.java
@@ -1,6 +1,8 @@
 package it.restchallenge.data;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.util.Calendar;
 
@@ -60,7 +62,7 @@ public class StoreData {
         Calendar dayOne = Calendar.getInstance();
         dayOne.setTime(DateTime.now().toDate());
         Calendar dayTwo = Calendar.getInstance();
-        dayOne.setTime(DateTime.parse(this.getOpenedDate()).toDate());
+        dayOne.setTime(DateTime.parse(this.getOpenedDate(), DateTimeFormat.forPattern("dd/MM/yyyy")).toDate());
 
         if (dayOne.get(Calendar.YEAR) == dayTwo.get(Calendar.YEAR)) {
             return Math.abs(dayOne.get(Calendar.DAY_OF_YEAR) - dayTwo.get(Calendar.DAY_OF_YEAR));
