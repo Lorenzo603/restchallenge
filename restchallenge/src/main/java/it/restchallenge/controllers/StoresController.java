41d144b462f0fa2240fe07149277d3ac8fb001a5
Lorenzo603 <lorenzo.furrer@gmail.com>
2017-06-24 10:33:12 +0200

    stores controller endpoint for get all stores sorted

--- restchallenge/src/main/java/it/restchallenge/controllers/StoresController.java
+++ restchallenge/src/main/java/it/restchallenge/controllers/StoresController.java
@@ -2,9 +2,12 @@ package it.restchallenge.controllers;
 
 import it.restchallenge.data.StoreData;
 import it.restchallenge.facades.StoreFacade;
+import it.restchallenge.options.StoreSortBy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Lfurrer on 24/06/2017.
@@ -15,11 +18,18 @@ public class StoresController {
     @Autowired
     private StoreFacade storeFacade;
 
+    @RequestMapping(value = "/stores/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.OK)
+    public StoreData getStore(@PathVariable final String id) {
+        return storeFacade.getStoreById(id);
+    }
+
     @RequestMapping(value = "/stores", method = RequestMethod.GET)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.OK)
-    public StoreData getStore(@RequestParam String id) {
-        return storeFacade.getStoreById(id);
+    public List<StoreData> getStoresSorted(@RequestParam String order) {
+        return storeFacade.getAllStoresSorted(StoreSortBy.valueOf(order));
     }
 
 }
